e1c78adfdc72be0d237f05e762c35d58f930d930
Rohit Ravoori <rohit.ravoori@gmail.com>
2016-12-22 00:02:01 -0500

    Query3 and 4

--- queries.py
+++ queries.py
@@ -1,12 +1,40 @@
 from dbhelper import DBHelper
+import datetime
 
 dbh = DBHelper()
 data = dbh.get_data()
 print "here"
 non_virtual_station = []
-for datum in data:
+'''for datum in data:
 #	if datum["properties"]["addressZipCode"] = str(19102):
 	val = str(datum["properties"]["addressZipCode"])
 	if val == "19104" :
 		print datum["properties"]["addressZipCode"]
+'''
+
+
+def query3(intime):
+	for datum in data:
+		val1 = (datum["properties"]["openTime"])
+		val2 = (datum["properties"]["closeTime"])
+		time1 = datetime.datetime.strptime(intime, '%H:%M:%S')
+		time2 = datetime.datetime.strptime(val1, '%H:%M:%S')
+		time3 = datetime.datetime.strptime(val2, '%H:%M:%S')
+		if (time1 > time2) and (time1 < time3):
+			print datum["properties"]["name"]
+			print datum["properties"]["addressStreet"]		
+	return
     
+
+query3("23:59:00")
+
+
+def query4():
+	for datum in data:
+		val1 = (datum["properties"]["name"])
+		if val1 == "penn" :
+			print datum["properties"]["name"]
+			print datum["properties"]["addressStreet"]	
+	return
+
+#query4()
